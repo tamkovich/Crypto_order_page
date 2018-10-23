840f1a04355fddeae0edbf11ae92119f1200cb14
Denis Tamkovich <jaselnikpromise@gmail.com>
2018-10-23 09:10:34 +0300

    login urls and templates

--- app.py
+++ app.py
@@ -1,12 +1,10 @@
 from gevent import monkey
-from flask import (
-    Flask, render_template,
-    request
-)
+from flask import Flask, render_template, request, redirect, url_for, flash
+from flask_login import LoginManager, login_user, current_user, logout_user, login_required, UserMixin
 from flask_socketio import SocketIO, emit
 from flask_sqlalchemy import SQLAlchemy
 from threading import Lock
-from forms import ClientForm
+from forms import ClientForm, UserLoginForm
 from Order import Client, update_client_data, check_for_blank_in_json_by_fields
 from db import POSTGRES
 import asyncio
@@ -16,6 +14,18 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://%(user)s:\
 %(pw)s@%(host)s:%(port)s/%(db)s' % POSTGRES
 db = SQLAlchemy(app)
+login_manager = LoginManager(app)
+
+
+class User(db.Model):
+    __tablename__ = 'UserClient'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String(100), unique=True)
+
+
+db.create_all()
+db.session.commit()
 
 
 class ClientModel(db.Model):
@@ -64,8 +74,36 @@ thread = None
 #    db.session.commit()
 #    db.create_all()
 
+@login_manager.user_loader
+def load_user(userid):
+    return User.query.get(userid)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = UserLoginForm()
+    if request.method == 'POST':
+        if current_user.is_authenticated:
+            return redirect(url_for('index'))
+        if form.validate():
+            user = User.query.filter_by(username=form.username.data).first()
+            if user is None or not user.check_password(form.password.data):
+                flash('Invalid username or password')
+                return redirect(url_for('login'))
+            login_user(user, remember=form.remember_me.data)
+            return redirect(url_for(''))
+    return render_template('login.html', title='Sign In', form=form)
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
+
 
 @app.route('/')
+@login_required
 def hello_world():
     form = ClientForm(request.form)
     return render_template('index.html', async_mode=socketio.async_mode, form=form)
--- forms.py
+++ forms.py
@@ -1,7 +1,11 @@
-from wtforms import Form, StringField, validators
+from wtforms import Form, StringField, validators, PasswordField
 
 
 class ClientForm(Form):
     apiKey = StringField('apiKey', [validators.Length(min=4, max=100)])
     secret = StringField('secret', [validators.Length(min=6, max=100)])
 
+
+class UserLoginForm(Form):
+    username = StringField('Username', [validators.data_required(), validators.Length(min=4, max=25)])
+    password = PasswordField('Password', [validators.data_required(), validators.Length(min=6, max=200)])
